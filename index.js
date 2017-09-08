bffda856c31609c11a92e72f5b3a0b310811d74e
rlrh1996 <rlrh1996@gmail.com>
2017-09-08 09:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function divide(a, b) {
 }
 
 function inc(n) {
-    n += 1
+    return n += 1
 }
 
 function dec(n) {
-    n -= 1
+    return n -= 1
 }
 
 function makeInt(n) {
-    parseInt(n, 10)
+    return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-    parseFloat(n)
+    return parseFloat(n)
 }
